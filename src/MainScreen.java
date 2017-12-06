9964c401e6875ea18c3e1b7e2707d2fce3b47ac2
Yago Pessoa <yagorp15@gmail.com>
2017-12-06 12:31:28 -0200

    Adicionado barra de menu e algumas melhorias

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -1,13 +1,19 @@
-import java.awt.EventQueue;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class MainScreen {
 
 	private JFrame frame;
 	private JPanel botoes;
+	private JMenu menu;
+	private JMenuBar barraMenu;
+	private JMenuItem testeRelatorio, sobre;
+	
+	private String strSobre = "Protótipo para manipulação de base de dados\n\n"
+			+ "Feito por:\nAlef Segura\nGustavo Moura\nThaís Lima\nYago Pessoa\n\n"
+			+ "Disciplina Base de Dados, 2017-2 - Prof. Robson";
+	
 
 	/**
 	 * Launch the application.
@@ -37,11 +43,29 @@ public class MainScreen {
 	 */
 	private void initialize() {
 		frame = new JFrame("Streaming de Video");
-		frame.setBounds(100, 100, 800, 300);
+		frame.setBounds(100, 100, 600, 800);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLocationRelativeTo(null);
 		
-		GridLayout mainLayout = new GridLayout(2, 1);
+		barraMenu = new JMenuBar();
+		menu = new JMenu("Opções");
+		testeRelatorio = new JMenuItem("Gerar relatório");
+		testeRelatorio.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// implementar a consulta na base de dados
+			}
+		});
+		sobre = new JMenuItem("Sobre");
+		sobre.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, strSobre);
+			}
+		});
+		menu.add(testeRelatorio);
+		menu.add(sobre);
+		barraMenu.add(menu);
+		frame.setJMenuBar(barraMenu);
+		
+		BoxLayout mainLayout = new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS);
 		frame.setLayout(mainLayout);
 
 		FlowLayout botoesLayout = new FlowLayout();
@@ -50,25 +74,32 @@ public class MainScreen {
 		frame.add(botoes);
 		
 		JButton btnCadastrar = new JButton("Cadastrar");
-		botoes.add(btnCadastrar);
-		
+		botoes.add(btnCadastrar);	
 		JButton btnAlterar = new JButton("Alterar");
 		botoes.add(btnAlterar);
-		
 		JButton btnExcluir = new JButton("Excluir");
 		botoes.add(btnExcluir);
 		
-		// TESTE TABELA
+		// TABELA TESTE
 		String [] colunas = {"Nome", "Telefone", "Email"};
 		Object [][] dados = {
-				{"Ana Monteiro", "48 9923-7898", "ana.monteiro@gmail.com"},
-				{"João da Silva", "48 8890-3345", "joaosilva@hotmail.com"},
-				{"Pedro Cascaes", "48 9870-5634", "pedrinho@gmail.com"}
+				{"Maria da Silva", "11 1111-1111", "maria@gmail.com"},
+				{"José Luiz", "22 2222-2222", "jose@gmail.com"},
+				{"Yago Pessoa", "33 3333-3333", "yago@gmail.com"},
+				{"Maria da Silva", "11 1111-1111", "maria@gmail.com"},
+				{"José Luiz", "22 2222-2222", "jose@gmail.com"},
+				{"Yago Pessoa", "33 3333-3333", "yago@gmail.com"},
+				{"Maria da Silva", "11 1111-1111", "maria@gmail.com"},
+				{"José Luiz", "22 2222-2222", "jose@gmail.com"},
+				{"Yago Pessoa", "33 3333-3333", "yago@gmail.com"}
 		};
 		
 		JTable tabela = new JTable(dados, colunas);
 		JScrollPane barraRolagem = new JScrollPane(tabela);
 		frame.add(barraRolagem);
+		
+		frame.pack();
+		frame.setLocationRelativeTo(null);
 	}
 
 }
