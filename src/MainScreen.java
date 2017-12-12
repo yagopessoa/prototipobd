99ed649107f2e4917a7ce8757406abed9faf1e1d
Yago Pessoa <yagorp15@gmail.com>
2017-12-12 17:08:01 -0200

    Alterar tupla OK

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -180,7 +180,11 @@ public class MainScreen {
 		btnAlterar = new JButton("Alterar");
 		btnAlterar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// implementar update de tupla aqui
+				try {
+					String codigo = tabela.getValueAt(tabela.getSelectedRow(), 0).toString();
+					btnAlterar.setEnabled(false);
+					atualizaTupla(codigo);
+				} catch (Exception ex) {}
 			}
 		});
 		botoes.add(btnAlterar);
@@ -314,8 +318,6 @@ public class MainScreen {
 		JButton confirma = new JButton("Confirmar");
 		confirma.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// pegar os dados dos campos e fazer a operacao de insercao de tupla
-				
 				try {
 					String select = "SELECT codigo FROM video WHERE (titulo = '" + videosJB.getSelectedItem().toString() + "')";
                     System.out.println(select);
@@ -325,8 +327,6 @@ public class MainScreen {
                     rs.next();
 					String cod_filme = rs.getString(1);
                     System.out.println(cod_filme);
-
-//					String cod_filme = "10";	// TESTEEEEE
 					
 					String sql = "INSERT INTO filme (video, sinopse, faixa_etaria, duracao, ano, idioma, legenda)"
 							+ " VALUES ("+cod_filme+", '"+txtSinopse.getText()+"', '"+faixaEtJB.getSelectedItem().toString()+"', '"+txtDuracao.getText()
@@ -371,26 +371,166 @@ public class MainScreen {
 		cadastro.setVisible(true);
 	}
 	
+	public void atualizaTupla(String codigo) {
+
+		JFrame cadastro = new JFrame("Atualizar cadastro");
+		cadastro.setBounds(100, 100, 700, 250);
+		//cadastro.setAlwaysOnTop(true);
+		cadastro.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		cadastro.setResizable(false);
+		
+		BoxLayout cadastroLay = new BoxLayout(cadastro.getContentPane(), BoxLayout.Y_AXIS);
+		cadastro.getContentPane().setLayout(cadastroLay);
+		
+		JPanel campos = new JPanel();
+		GridLayout camposLay = new GridLayout(7, 2);
+		campos.setLayout(camposLay);
+		
+		JLabel videosLabel = new JLabel("Videos sem cadastro:");
+		videosLabel.setFont(new Font("Arial", 1, 14));
+		campos.add(videosLabel);
+		
+		boolean erro = false;
+		try {
+			String select = "SELECT titulo FROM video WHERE (codigo = '" + codigo + "')";
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(select);
+            String titulo;
+            rs.next();
+       		titulo = rs.getString("titulo");
+       		
+    		JLabel tituloLabel = new JLabel(titulo);
+    		videosLabel.setFont(new Font("Arial", 1, 14));
+    		campos.add(tituloLabel);
+		} catch (Exception ex) {
+			erro = true;
+			cadastro.dispose();
+			JOptionPane.showMessageDialog(frame, "Erro: "+ex.getMessage());
+		}	
+		
+		JLabel sinopseLabel = new JLabel("Sinopse:");
+		sinopseLabel.setFont(new Font("Arial", 1, 14));
+		campos.add(sinopseLabel);
+		
+		JTextField txtSinopse = new JTextField(14);
+		txtSinopse.setFont(new Font("Arial", 1, 14));
+		campos.add(txtSinopse);
+		
+		JLabel faixaEtLabel = new JLabel("Classificação Etaria:");
+		faixaEtLabel.setFont(new Font("Arial", 1, 14));
+		campos.add(faixaEtLabel);
+		
+		final JComboBox<String> faixaEtJB = new JComboBox<>(listaFaixaEt);
+		campos.add(faixaEtJB);
+		
+		JLabel duracaoLabel = new JLabel("Duracao (min):");
+		duracaoLabel.setFont(new Font("Arial", 1, 14));
+		campos.add(duracaoLabel);
+		
+		JTextField txtDuracao = new JTextField(14);
+		txtDuracao.setFont(new Font("Arial", 1, 14));
+		campos.add(txtDuracao);
+		
+		JLabel anoLabel = new JLabel("Ano:");
+		anoLabel.setFont(new Font("Arial", 1, 14));
+		campos.add(anoLabel);
+		
+		JTextField txtAno = new JTextField(14);
+		txtAno.setFont(new Font("Arial", 1, 14));
+		campos.add(txtAno);
+		
+		JLabel idiomaLabel = new JLabel("Idioma:");
+		idiomaLabel.setFont(new Font("Arial", 1, 14));
+		campos.add(idiomaLabel);
+		
+		final JComboBox<String> idiomaJB = new JComboBox<>(listaIdioma);
+		campos.add(idiomaJB);
+		
+		JLabel legendaLabel = new JLabel("Legenda:");
+		legendaLabel.setFont(new Font("Arial", 1, 14));
+		campos.add(legendaLabel);
+		
+		final JComboBox<String> legendaJB = new JComboBox<>(listaLegenda);
+		campos.add(legendaJB);
+		
+		try {
+			String select = "SELECT * FROM filme WHERE (video = '" + codigo + "')";
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(select);
+            rs.next();
+       		txtSinopse.setText(rs.getString("sinopse"));
+       		faixaEtJB.setSelectedItem(rs.getString("faixa_etaria"));
+       		String duracao = rs.getString("duracao");
+       		txtDuracao.setText(duracao.substring(0, duracao.length()-3));
+       		txtAno.setText(rs.getString("ano"));
+       		idiomaJB.setSelectedItem(rs.getString("idioma"));
+       		legendaJB.setSelectedItem(rs.getString("legenda"));
+		} catch (Exception ex) {
+			erro = true;
+			cadastro.dispose();
+			JOptionPane.showMessageDialog(frame, "Erro: "+ex.getMessage());
+		}
+		
+		
+		JPanel botoes = new JPanel();
+		FlowLayout botoesLay = new FlowLayout();
+		botoes.setLayout(botoesLay);
+		
+		JButton confirma = new JButton("Confirmar");
+		confirma.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					String sql = "UPDATE filme SET sinopse='"+txtSinopse.getText()+"', faixa_etaria='"+faixaEtJB.getSelectedItem().toString()+"', "
+							+ "duracao='"+txtDuracao.getText()+"min', ano='"+txtAno.getText()+"', idioma='"+idiomaJB.getSelectedItem().toString()+"', "
+							+ "legenda='"+legendaJB.getSelectedItem().toString()+"' WHERE video='"+codigo+"'";
+                    System.out.println(sql);
+
+					pstmt = connection.prepareStatement(sql);
+	                try{
+	                    pstmt.executeUpdate();
+	    				cadastro.dispose();
+						JOptionPane.showMessageDialog(frame, "Dados inseridos!");
+	                    pstmt.close();
+						updateTable();
+	                } catch (SQLException ex) {
+	    				cadastro.dispose();
+						JOptionPane.showMessageDialog(frame, "Dados NAO inseridos! Erro: "+ex.getMessage());
+	                }
+				} catch (Exception ex) {
+					JOptionPane.showMessageDialog(frame, "Erro: "+ex.getMessage());
+				}
+				
+				btnAlterar.setEnabled(true);
+				frame.setVisible(true);
+			}
+		});
+		botoes.add(confirma);
+		
+		JButton cancela = new JButton("Cancelar");
+		cancela.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cadastro.dispose();
+				btnAlterar.setEnabled(true);
+				frame.setVisible(true);
+			}
+		});
+		botoes.add(cancela);
+
+		cadastro.add(campos);
+		cadastro.add(botoes);
+		cadastro.pack();
+		cadastro.setLocationRelativeTo(frame);
+		if(!erro) cadastro.setVisible(true);
+	}
+	
 	public void updateTable() {
 		try {
-			/*SELECAO*/
             String sql = "SELECT * FROM filme";
             stmt = connection.createStatement();
             rs = stmt.executeQuery(sql);
 		    tabela.setModel(DbUtils.resultSetToTableModel(rs));
 		    barraRolagem.repaint();
 		    frame.repaint();
-		    /*
-            while (rs.next()) {
-                System.out.println(rs.getString("video") + "-"
-                        + rs.getString("sinopse") + "-"
-                        + rs.getString("faixa_etaria") + "-"
-                        + rs.getString("duracao") + "-"
-                        + rs.getString("ano") + "-"
-                        + rs.getString("idioma") + "-"
-                        + rs.getString("legenda")
-                        );
-            }*/
 		} catch (Exception ex) {
 			JOptionPane.showMessageDialog(frame, "Erro: "+ex.getMessage());
 		}
